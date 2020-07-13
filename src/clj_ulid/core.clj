ee6ff879f7dc2470bbf1c09d15b9b70d76229945
John Collins <john.collins@braincorp.com>
2020-07-12 22:33:36 -0700

    String -> ULID function

--- src/clj_ulid/core.clj
+++ src/clj_ulid/core.clj
@@ -1,8 +1,6 @@
 (ns clj-ulid.core
-  (:import de.huxhorn.sulky.ulid.ULID
-           de.huxhorn.sulky.ulid.ULID$Value
-           java.util.UUID
-           [java.security MessageDigest]
+  (:import [de.huxhorn.sulky.ulid ULID ULID$Value]
+           [java.util UUID]
            [java.nio ByteBuffer]))
 
 (defonce ^:private ulid-instance (ULID.))
@@ -34,6 +32,11 @@
   [^String s]
   (ULID/parseULID s))
 
+(defn string->uuid
+  "Creates a UUID form a string."
+  [^String s]
+  (UUID/fromString s))
+
 (defn bytes->ulid
   "converts a ULID from binary representation to string representation"
   ^String
@@ -45,14 +48,14 @@
   [^ULID id]
   (.toBytes id))
 
-(defn ulid->uuid ^java.util.UUID [^ULID ulid]
+(defn ulid->uuid ^UUID [^ULID ulid]
   (let [bytes (.toBytes ulid)
         bb (ByteBuffer/wrap bytes)
         low (.getLong bb)
         hi (.getLong bb)]
     (UUID. low hi)))
 
-(defn uuid->ulid ^ULID [^java.util.UUID uuid]
+(defn uuid->ulid ^ULID [^UUID uuid]
   (let [hi (.getMostSignificantBits uuid)
         low (.getLeastSignificantBits uuid)
         buf (ByteBuffer/allocate (* 2 Long/BYTES))]
